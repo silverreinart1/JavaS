88ed84f5b85c8abeaa760b3a2acfd63f1b4b2d03
silverreinart1 <silverreinart01@gmail.com>
2023-11-16 16:52:01 +0200

    first commit

---
+++ loop.js
@@ -0,0 +1,4 @@
+let names = ["bmw", "Mercedes-Benz", "volvo", "audi", "ford", "MINI", "jaguar", "kia", "hyundai", "volkswagen", "toyota", "renault", "opel", "skoda", "saab", "nissan", "mazda", "subaru", "honda", "fiat", "lada", "lexus", "jeep", "dacia"];
+for (let i in names) {
+  console.log(names[i]);
+}
